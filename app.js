8729cc327602eabaf3594d484d1cccdafee39170
viniduminsara <viniduminsara@gmail.com>
2023-12-04 21:52:19 +0530

    added bootstrap and ejs layout

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -19,7 +21,18 @@ app.listen(port, () => {
     console.log('Server up at port 3000');
 });
 
-//defining routes
+//define routes
 app.get('/', async(req, res) => {
     res.render('home');
 });
+
+app.get('/campgrounds', async(req, res) => {
+    res.render('campgrounds/index');
+});
+
+
+
+
+
+
+
