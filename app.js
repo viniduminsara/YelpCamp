658713159112a68d84efaf0c38c3c947ee0e15ee
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-12-10 05:21:35 +0530

    config port

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.use(
     })
 );
 
-const port = 80;
+const port = process.env.PORT || 3000;
 const db = mongoose.connection;
 
 //connect with database
@@ -97,7 +97,7 @@ db.on('error', console.error.bind(console, 'Database connection error :('));
 db.once('open', () => console.log('Database connected :)'));
 
 app.listen(port, () => {
-    console.log('Server up at port 80');
+    console.log(`Server up at port ${port}`);
 });
 
 app.use((req, res, next) => {
